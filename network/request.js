f2d32f0ebd37c0c71435f8e5beb3a953521f46c1
xzwc <1318896373@qq.com>
2019-11-19 16:42:51 +0800

    fix

--- network/request.js
+++ network/request.js
@@ -2,17 +2,33 @@ var Fly = require("../lib/net/wx.umd.min.js")
 var request = new Fly()
 
 request.interceptors.request.use((request) => {
-    return request
+	if (app.authToken) {
+		request.headers = {
+			"AUTHORIZATION": `Token token=${app.authToken}`
+		}
+	} else {
+		try {
+			var authToken = wx.getStorageSync('authToken')
+			if (authToken) {
+				request.headers = {
+					"AUTHORIZATION": `Token token=${authToken}`
+				}
+			}
+		} catch (e) {
+			console.log(e)
+		}
+	}
+	return request
 })
 
 request.interceptors.response.use(
-    (response, promise) => {
-        return promise.resolve(response.data)
-    },
-    (error, promise) => {
-        // return promise.reject()
-        return promise.resolve(error)
-    }
+	(response, promise) => {
+		return promise.resolve(response.data)
+	},
+	(error, promise) => {
+		// return promise.reject()
+		return promise.resolve(error)
+	}
 )
 
 export default request
