950dc49e36a535efed439a6d1fe7deab9928e6d8
xzwc <1318896373@qq.com>
2019-11-20 15:56:49 +0800

    设置缓存模块

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,10 @@
 const app = getApp()
 import wxNav from '../../utils/nav.js'
 import Logger from '../../utils/logger.js'
-const logger = new Logger.getLogger('pages/index/index');
+const logger = new Logger.getLogger('pages/index/index')
+
+import storage from '../../utils/storage.js'
+
 
 Page({
   data: {
@@ -10,15 +13,18 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
+ 
+	//事件处理函数
   bindViewTap: function() {
     wxNav.navigateTo( '/pages/logs/logs')
   },
-  onLoad: function () {
+	
+	onLoad: function () {
 		logger.error('123456789')
 		logger.warn('123456789')
 		logger.info('1234567890000','6789')
 		logger.debug('123456789')
+		storage.put('hello','123')
 	 
 		this.longToast = new app.weToast()
     this.longToast.toast({
