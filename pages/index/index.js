1dce904b194a216f48452300f07892fd0d4b4aaa
zyfee <zhaoyf@gongfudou.com>
2019-11-22 09:47:17 +0800

    page mixins

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,33 +3,39 @@ import wxNav from '../../utils/nav.js'
 import Logger from '../../utils/logger.js'
 const logger = new Logger.getLogger('pages/index/index')
 
+// page mixins
+require('../../utils/mixin.js')
+import index from "../../mixins/index.js"
+import init from "../../mixins/init.js"
+
 import storage from '../../utils/storage.js'
 
-
 Page({
+  mixins: [index, init],
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
- 
-	//事件处理函数
+
+  //事件处理函数
   bindViewTap: function() {
-    wxNav.navigateTo( '/pages/logs/logs')
+    wxNav.navigateTo('/pages/logs/logs')
   },
-	
-	onLoad: function () {
-		logger.error('123456789')
-		logger.warn('123456789')
-		logger.info('1234567890000','6789')
-		logger.debug('123456789')
-		storage.put('hello','123')
-	 
-		this.longToast = new app.weToast()
+
+  onLoad: function() {
+    logger.error('123456789')
+    logger.warn('123456789')
+    logger.info('1234567890000', '6789')
+    logger.debug('123456789')
+    storage.put('hello', '123')
+
+    console.log('initMsg=====', this.data.initMsg)
+    this.longToast = new app.weToast()
     this.longToast.toast({
-      type:"loading",
-      duration:3000
+      type: "loading",
+      duration: 3000
     })
-	}
+  }
 })
