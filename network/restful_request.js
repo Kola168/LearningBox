a021bbaf51b8397ef94411767b0f15279b508d5e
Alice <Lis@gongfudou.com>
2019-12-03 10:41:30 +0800

    首页

--- network/restful_request.js
+++ network/restful_request.js
@@ -1,15 +1,18 @@
 var app = getApp()
 import request from './request'
-
 const api = {
-	
   covertInvoiceToPdf: (openId, convertUrls) => request.post(`orders/invoice_url_convert`, {
     openid: openId,
     pdf_urls: convertUrls
   }, {
     baseURL: `${app.apiServer}/ec/v2/`
   }),
-  
+  wechatDecryption: (params) => request.post(`users/wechat_decryption`, {
+   params
+  }, {
+    baseURL: `${app.apiServer}/api/v1/`
+  })
+
 }
 
 export default api
