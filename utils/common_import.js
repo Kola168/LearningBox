e0e4b3a9e3488a9f719b0fe240b0a8c9565b009e
xzwc <1318896373@qq.com>
2019-12-04 16:35:50 +0800

    fix

---
+++ utils/common_import.js
@@ -0,0 +1,8 @@
+const regeneratorRuntime = require('../lib/co/runtime')
+const co = require('../lib/co/co')
+const util = require('./util')
+const _ = require('../lib/underscore/we-underscore')
+const uploadFormId = require('./gfd-formid-upload')
+const common_util = require('./common_util')
+
+export { regeneratorRuntime, co, util, _, uploadFormId, common_util }
