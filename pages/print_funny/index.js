190971f1eea3b8068ba7f336ed4e3175f9891dd6
xzwc <1318896373@qq.com>
2019-12-09 16:06:46 +0800

    趣味打印首页

--- pages/print_funny/index.js
+++ pages/print_funny/index.js
@@ -1,79 +1,138 @@
+var app = getApp()
+import router from '../../utils/nav'
+
 Page({
-  data: {
+	data: {
 		funApplications: [{
-        name: '气球打印',
-        icon: '/images/funny_ballon.png',
-        key: 'ballon'
-      },
-      {
-        name: '照片书',
-        icon: '/images/doc_more_icon.png',
-        url: '',
-        key: 'photoBook'
+			name: '气球打印',
+			icon: '/images/funny_ballon.png',
+			key: 'ballon'
+		},
+		{
+			name: '照片书',
+			icon: '/images/doc_more_icon.png',
+			url: '',
+			key: 'photoBook'
+		},
+		{
+			name: '台历',
+			icon: '/images/funny_calendar.png',
+			url: '',
+			key: 'calendar'
+		},
+		{
+			name: '拇指相册',
+			icon: '/images/funny_thumb_album.png',
+			url: '',
+			key: 'thumbAlum'
+		},
+		{
+			name: '大头贴',
+			icon: '/images/funny_sticker.png',
+			url: '/pages/print_doc/function_list/function_list',
+			key: 'sticker'
+		},
+		{
+			name: '姓名贴',
+			icon: '/images/funny_name.png',
+			url: '',
+			key: 'name'
+		},
+		{
+			name: '明信片',
+			icon: '/images/funny_postcard.png',
+			url: '',
+			key: 'postcard'
+		}
+		],
+		smartApplications: [
+			{
+				name: '图片转文档',
+				icon: '/images/print_pic_to_a4.png',
+				key: 'picToA4',
+				url: '/pages/print_photo_doc/index',
 			},
 			{
-        name: '台历',
-        icon: '/images/doc_more_icon.png',
-        url: '',
-        key: 'calendar'
+				name: '公众号文章',
+				icon: '/images/doc_official_icon.png',
+				key: 'weChatArticle',
+				url: ''
 			},
 			{
-        name: '拇指相册',
-        icon: '/images/funny_thumb_album.png',
-        url: '',
-        key: 'thumbAlum'
+				name: '复印',
+				icon: '/images/doc_copy_icon.png',
+				key: 'copy',
+				url: '/pages/print_doc/duplicate/index',
 			},
 			{
-        name: '大头贴',
-        icon: '/images/funny_sticker.png',
-        url: '/pages/print_doc/function_list/function_list',
-        key: 'sticker'
-			},
-			{
-        name: '姓名贴',
-        icon: '/images/funny_name.png',
-        url: '',
-        key: 'name'
-			},
-			{
-        name: '明信片',
-        icon: '/images/funny_postcard.png',
-        url: '',
-        key: 'postcard'
-      }
-    ],
-    smartApplications: [
-			{
-        name: '图片转文档',      
-        icon: '/images/print_pic_to_a4.png',
-        key: 'picToA4',
-        url: '/pages/print_photo_doc/index',
-			},
-			{
-        name: '公众号文章',
-        icon: '/images/doc_official_icon.png',
-        key: 'weChatArticle',
-        url: ''
-      },
-			{
-        name: '复印',      
-        icon: '/images/doc_copy_icon.png',
-        key: 'copy',
-        url: '/pages/print_doc/duplicate/index',
-      },
-      {
-        name: '共享文件夹',
-        icon: '/images/share_folder.png',
+				name: '共享文件夹',
+				icon: '/images/share_folder.png',
 				key: 'shareFolder',
 				url: '/pages/print_doc/duplicate/index',
-      },
-      {
-        name: '电子发票',
-        icon: '/images/doc_invoice_icon.png',
-        key: 'invoice',
-        url: '/pages/print_doc/print_invoice/print_invoice'
-      }
-    ]
-  },
+			},
+			{
+				name: '电子发票',
+				icon: '/images/doc_invoice_icon.png',
+				key: 'invoice',
+				url: '/pages/print_doc/print_invoice/print_invoice'
+			}
+		]
+	},
+
+	onLoad: function () {
+
+	},
+
+	//趣味打印
+	toFunApplication: function (e) {
+		let type = e.currentTarget.dataset.key
+		let url = e.currentTarget.dataset.url
+		switch (e.currentTarget.dataset.key) {
+			case 'ballon':
+				router.navigateTo(url)
+				break
+			case 'photoBook':
+				router.navigateTo(url)
+				break
+			case 'calendar':
+				router.navigateTo(url)
+				break
+			case 'thumbAlum':
+				router.navigateTo(url)
+				break
+			case 'sticker':
+				router.navigateTo(url)
+				break
+			case 'name':
+				router.navigateTo(url)
+				break
+			case 'calepostcardndar':
+				router.navigateTo(url)
+				break
+		}
+	},
+
+	//智能应用
+	toSmartApplication: function (e) {
+		let type = e.currentTarget.dataset.key
+		let url = e.currentTarget.dataset.url
+		switch (e.currentTarget.dataset.key) {
+			case 'picToA4':
+				router.navigateTo(url)
+				break
+			case 'weChatArticle':
+				router.navigateTo(url)
+				break
+			case 'copy':
+				router.navigateTo(url)
+				break
+			case 'shareFolder':
+				router.navigateTo(url)
+				break
+			case 'invoice':
+				router.navigateTo(url)
+				break
+		}
+	}
 
 })
