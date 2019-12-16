9415cb1f723c3a887cda0de4b9168413dcfc8949
zyfee <zhaoyf@gongfudou.com>
2019-12-16 16:49:32 +0800

    优化免费资源库

--- pages/package_common/free_resources/index/index.js
+++ pages/package_common/free_resources/index/index.js
@@ -16,46 +16,45 @@ Page({
     // 左 => 右联动 右scroll-into-view 所需的id
     toViewId: null,
     // 当前左侧选择的
-    currentLeftSelect: null,
+    currentLeftSelect: 'id0',
     leftToTop: 0
   },
-  onLoad: function () {
+  onLoad: function() {
     // 右侧每类数据到顶部的距离（用来与 右 => 左 联动时监听右侧滚动到顶部的距离比较）
     this.eachRightItemToTop = []
-
-    let screenWidth = app.sysInfo.screenWidth,
-      rightItemHeight = 140
-    if (screenWidth < 340) {
-      rightItemHeight = 110
-    } else if (screenWidth >= 410) {
-      rightItemHeight = 150
-    }
-    this.setData({
-      minHeight: app.sysInfo.screenHeight - app.navBarInfo.topBarHeight,
-      rightItemHeight,
-      constants: constants,
-      currentLeftSelect: constants[0].id
-    })
-    this.eachRightItemToTop = this.getEachRightItemToTop()
+    setTimeout(() => {
+      let screenWidth = app.sysInfo.screenWidth,
+        rightItemHeight = 140
+      if (screenWidth < 340) {
+        rightItemHeight = 110
+      } else if (screenWidth >= 410) {
+        rightItemHeight = 150
+      }
+      this.setData({
+        minHeight: app.sysInfo.screenHeight - app.navBarInfo.topBarHeight,
+        rightItemHeight,
+        constants: constants,
+        currentLeftSelect: constants[0].id
+      })
+      this.eachRightItemToTop = this.getEachRightItemToTop()
+      console.log(this.eachRightItemToTop)
+    }, 500)
   },
 
   // 获取每个右侧的 bar 到顶部的距离，用来做后面的计算。
-  getEachRightItemToTop: function () {
+  getEachRightItemToTop: function() {
     try {
       let obj = {},
         totop = 0
 
       // 右侧第一类肯定是到顶部的距离为 0
       obj[constants[0].id] = totop
-      // 循环来计算每个子类到顶部的高度
-      for (let i = 1; i < (constants.length + 1); i++) {
-        let rowCount = Math.ceil(constants[i - 1].category.length / 3)
-        console.log('rowCount', rowCount)
-        console.log('this.data.rightItemHeight', this.data.rightItemHeight)
+        // 循环来计算每个子类到顶部的高度
+      for (let i = 0; i < constants.length; i++) {
+        let rowCount = Math.ceil(constants[i].category.length / 3)
         totop += (RIGHT_BAR_HEIGHT + rowCount * this.data.rightItemHeight)
-        console.log('totop', totop)
-        // 这个的目的是 例如有两类，最后需要 0-1 1-2 2-3 的数据，所以需要一个不存在的 'last' 项，此项即为第一类加上第二类的高度。
-        obj[constants[i] ? constants[i].id : 'last'] = totop
+          // 这个的目的是 例如有两类，最后需要 0-1 1-2 2-3 的数据，所以需要一个不存在的 'last' 项，此项即为第一类加上第二类的高度。
+        obj[constants[i + 1] ? constants[i + 1].id : 'last'] = totop
       }
       return obj
     } catch (error) {
@@ -64,11 +63,14 @@ Page({
 
   },
   // 监听右侧的滚动事件与 eachRightItemToTop 的循环作对比 从而判断当前可视区域为第几类，从而渲染左侧的对应类。
-  rightScroll: function (e) {
+  rightScroll: function(e) {
     for (let i = 0; i < this.data.constants.length; i++) {
-      let left = this.eachRightItemToTop[this.data.constants[i].id]
-      let right = this.eachRightItemToTop[this.data.constants[i + 1] ? this.data.constants[i + 1].id : 'last']
+      let tempId = this.data.constants[i].id,
+        tempNextId = this.data.constants[i + 1],
+        left = this.eachRightItemToTop[tempId],
+        right = this.eachRightItemToTop[tempNextId ? tempNextId.id : 'last']
       if (e.detail.scrollTop < right && e.detail.scrollTop >= left) {
+        if (this.data.currentLeftSelect === tempId) return
         this.setData({
           currentLeftSelect: this.data.constants[i].id,
           leftToTop: LEFT_ITEM_HEIGHT * i
@@ -76,11 +78,13 @@ Page({
       }
     }
   },
+
   // 左侧类的点击事件，点击时，右侧会滚动到对应分类
-  changeType: function (e) {
+  changeType: function(e) {
+    let id = e.currentTarget.dataset.id
     this.setData({
-      toViewId: e.target.id || e.target.dataset.id,
-      currentLeftSelect: e.target.id || e.target.dataset.id
+      toViewId: id,
+      currentLeftSelect: id
     })
   },
   toNext() {
