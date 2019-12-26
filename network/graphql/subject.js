bdbb145d8326e5f510b7dd05eabe14cdc2b8b215
zyfee <zhaoyf@gongfudou.com>
2019-12-26 13:36:02 +0800

    fix

--- network/graphql/subject.js
+++ network/graphql/subject.js
@@ -5,17 +5,61 @@ const graphqlApi = {
   /**
    * 注册学科网
    *
-   * @returns
    */
   register: () => {
     return gql.mutate({
       mutation: `mutation ($input: RegisterInput!){
-        Register{
+        Register(input:$input){
           state
+        }
+      }`,
+      variables: {
+        input: {
+
+        }
+      }
+    })
+  },
+
+  /**
+   * 获取学科
+   *
+   */
+  getSubjects: () => {
+    return gql.query({
+      query: `query {
+        xuekewang {
+          registered
+          subjects {
+            iconUrl
+            subjectId
+            subjectName
+            totalNumber
+          }
         }
       }`
     })
-  }
+  },
+
+  /**
+   * 获取学科教材版本
+   * @param { Number } subjectId
+   */
+  getSubjectsVersions: (subjectId) => {
+    return gql.query({
+      query: `query ($subjectId: Int!){
+        xuekewang {
+          textbookVersions(subjectId:$subjectId){
+            name
+            versionId
+          }
+        }
+      }`,
+      variables: {
+        subjectId: subjectId
+      }
+    })
+  },
 }
 
 export default graphqlApi
--- pages/package_subject/evaluate_exam/testpaper/index.js
+++ pages/package_subject/evaluate_exam/testpaper/index.js
@@ -1,39 +1,165 @@
+"use strict"
 const app = getApp()
+import { regeneratorRuntime, co, wxNav, util } from "../../../../utils/common_import"
+import gqlSubject from '../../../../network/graphql/subject'
 Page({
   data: {
     topBarHeight: 0,
     tabId: 'tab_0',
     typeSn: '',
-    showEditions: false,
+    showFilter: false,
+    activeFilter: {
+      sn: 1,
+      name: '江苏'
+    },
+    activeEdition: {
+      sn: 1,
+      name: '江苏'
+    },
+    activeGrade: {
+      sn: 1,
+      name: '七年级'
+    },
+    editions: [{
+      sn: 1,
+      name: '江苏'
+    }, {
+      sn: 2,
+      name: '浙江'
+    }, {
+      sn: 3,
+      name: '安徽'
+    }, {
+      sn: 4,
+      name: '河南'
+    }, {
+      sn: 5,
+      name: '上海'
+    }],
+    grades: [{
+      sn: 1,
+      name: '一年级'
+    }, {
+      sn: 2,
+      name: '二年级'
+    }, {
+      sn: 3,
+      name: '三年级'
+    }, {
+      sn: 4,
+      name: '四年级'
+    }, {
+      sn: 5,
+      name: '五年级'
+    }],
     papersTypes: [{
       sn: 1,
       name: '全部'
     }, {
-      sn: 1,
-      name: '期中'
+      sn: 2,
+      name: '期中试卷'
     }, {
-      sn: 1,
-      name: '期末'
+      sn: 3,
+      name: '期末试卷'
+    }, {
+      sn: 4,
+      name: '期中试卷'
+    }, {
+      sn: 5,
+      name: '期末试卷'
+    }, {
+      sn: 6,
+      name: '期中试卷'
+    }, {
+      sn: 7,
+      name: '期末试卷'
     }]
   },
-  onLoad() {
-    setTimeout(() => {
-      this.setData({
-        topBarHeight: app.navBarInfo.topBarHeight + 50
-      })
-    }, 300)
-  },
-  showEditions() {
+  onLoad(query) {
+    this.weToast = new app.weToast()
+    this.subjectId = Number(query.id)
     this.setData({
-      showEditions: !this.data.showEditions
+      topBarHeight: app.navBarInfo.topBarHeight + 50
+    })
+    this.getSubjectEditions()
+  },
+  showFilter(e) {
+    let id = e.currentTarget.id,
+      dataObj = {
+        showFilter: !this.data.showFilter
+      }
+    if (id === 'edition') {
+      dataObj.activeFilter = this.data.activeEdition
+    } else if (id === 'grade') {
+      dataObj.activeFilter = this.data.activeGrade
+    }
+    this.setData(dataObj)
+  },
+  // 切换版本
+  changeEdition(e) {
+    let index = e.currentTarget.dataset.index
+    this.setData({
+      activeEdition: this.data.editions[index]
     })
   },
+  // 切换试卷类型
   changeType(e) {
     let tabId = e.currentTarget.id,
       typeSn = e.currentTarget.dataset.sn
     this.setData({
-      typeSn,
-      tabId
+        typeSn,
+        tabId
+      })
+      // this.getTestpapers()
+  },
+  // 跳转详情
+  toDetail(e) {
+    if (app.preventMoreTap(e)) return
+    let sn = e.currentTarget.dataset.sn
+    wxNav.navigateTo('../../detail/index', {
+      sn: sn
     })
-  }
+  },
+
+  // 获取试卷版本
+  getSubjectEditions: co.wrap(function*() {
+    this.weToast.toast({
+      type: 'loading'
+    })
+    try {
+      let res = yield gqlSubject.getSubjectsVersions(this.subjectId)
+      console.log(res)
+      this.weToast.hide()
+    } catch (error) {
+      this.weToast.hide()
+      util.showError(error)
+    }
+  }),
+  // 获取试卷类型
+  getTestpapersType: co.wrap(function*() {
+    this.weToast.toast({
+      type: 'loading'
+    })
+    try {
+      let res = yield graphql.getTestpapersType(this.data.activeEdition.sn)
+      this.weToast.hide()
+    } catch (error) {
+      this.weToast.hide()
+      util.showError(error)
+    }
+  }),
+
+  // 获取试卷
+  getTestpapers: co.wrap(function*() {
+    this.weToast.toast({
+      type: 'loading'
+    })
+    try {
+      let res = yield graphql.getTestpapers(this.data.typeSn)
+      this.weToast.hide()
+    } catch (error) {
+      this.weToast.hide()
+      util.showError(error)
+    }
+  })
 })
