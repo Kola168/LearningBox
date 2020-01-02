84acf4bfd004c7b564d573c726eeb96d211b3021
yafee <ya_fee@163.com>
2020-01-02 19:08:47 +0800

    测评考试fix

--- network/graphql/subject.js
+++ network/graphql/subject.js
@@ -30,6 +30,8 @@ const graphqlApi = {
       query: `query {
         xuekewang {
           registered
+          printPaperCount
+          percentage
           subjects {
             iconUrl
             subjectId
