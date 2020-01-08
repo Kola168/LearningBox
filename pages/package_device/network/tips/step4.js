d3108dd1bf4341a8b6a2eb8bd6c3b7c35b84a03c
xzwc <1318896373@qq.com>
2020-01-08 11:39:47 +0800

    fix

---
+++ pages/package_device/network/tips/step4.js
@@ -0,0 +1,12 @@
+const app = getApp()
+const regeneratorRuntime = require('../../../../lib/co/runtime')
+const co = require('../../../../lib/co/co')
+const _ = require('../../../../lib/underscore/we-underscore')
+const util = require('../../../../utils/util')
+
+const request = util.promisify(wx.request)
+
+import wxNav from '../../../../utils/nav.js'
+Page({
+	
+})
