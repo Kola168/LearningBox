f7c610845ec5ad38865467379129098f289cc8df
xzwc <1318896373@qq.com>
2020-01-14 16:59:44 +0800

    放开入口

--- app.js
+++ app.js
@@ -21,21 +21,21 @@ App({
 	version: '0.0.2',
 	//线上地址
 	// apiServer: 'https://api.xbxxhz.com',
-	apiWbviewServer: 'https://api.xbxxhz.com',
+	// apiWbviewServer: 'https://api.xbxxhz.com',
 
 	//staging
-	// apiServer: 'https://lb-stg.gongfudou.com',
-	// apiWbviewServer: 'https://lb-stg.gongfudou.com',
+	apiServer: 'https://lb-stg.gongfudou.com',
+	apiWbviewServer: 'https://lb-stg.gongfudou.com',
 
 	//王析理本地地址
-	apiServer: 'http://epbox.natapp1.cc',
+	// apiServer: 'http://epbox.natapp1.cc',
 	// apiWbviewServer: 'http://epbox.natapp1.cc',
 
 	// 袁小飞接口
 	// apiServer: 'http://schaffer.utools.club',
 
 	//一个秃子的服务器地址
-	apiServer: 'http://jran.nat300.top',
+	// apiServer: 'http://jran.nat300.top',
 
 	// 测试接口季慧新
 	// apiServer: 'http://jhx.nat300.top',
