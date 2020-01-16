e21218da8355709cd7eaad980323e3a16cffd23c
yafee <ya_fee@163.com>
2020-01-16 19:16:26 +0800

    fix 知识图谱

--- pages/package_subject/super_errorbook/index/index.js
+++ pages/package_subject/super_errorbook/index/index.js
@@ -63,7 +63,7 @@ Page({
       for (let i = 0; i < rates.length; i++) {
         if (rates[i].questionNum > 0) {
           this.atlasData.title.push(rates[i].subjectName)
-          this.atlasData.data.push(rates[i].scoringRate === 0 ? 0.01 : this.atlasData.data[i])
+          this.atlasData.data.push(rates[i].scoringRate == 0 ? 0.01 : rates[i].scoringRate)
         }
       }
       if (this.atlasData.title.length > 2) {
