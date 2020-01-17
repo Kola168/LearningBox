97b7714407f0f6c3974d565bbd2c7bfb047413c4
周旭伟 <zhouxuwei@zhouxuweideMacBook-Pro.local>
2020-01-17 15:41:20 +0800

    获取手机号功能注释

---
+++ pages/package_feature/kousuan/teacher.js
@@ -0,0 +1,19 @@
+// pages/package_feature/kousuan/teacher.js
+Page({
+
+  data: {
+    urls: ['https://cdn-h.gongfudou.com/LearningBox/feature/kousuan_teacher1.png',
+      'https://cdn-h.gongfudou.com/LearningBox/feature/kousuan_teacher2.png',
+      'https://cdn-h.gongfudou.com/LearningBox/feature/kousuan_teacher3.png',
+      'https://cdn-h.gongfudou.com/LearningBox/feature/kousuan_teacher4.png',
+      'https://cdn-h.gongfudou.com/LearningBox/feature/kousuan_teacher5.png',
+      'https://cdn-h.gongfudou.com/LearningBox/feature/kousuan_teacher6.png',
+      'https://cdn-h.gongfudou.com/LearningBox/feature/kousuan_teacher7.png',
+    ],
+  },
+
+  onLoad: function (options) {
+
+  },
+
+})
