14af20bce7521d0fcd4b4f8f7005dc291232431e
yafee <ya_fee@163.com>
2020-01-20 17:29:15 +0800

    调整不同尺寸图片水印

--- pages/package_course/print/print.js
+++ pages/package_course/print/print.js
@@ -35,7 +35,7 @@ Page({
 
   zoomImg(e) {
     let index = Number(e.currentTarget.id),
-      url = this.data.lessonImgs[index].imgObj.url + '?x-image-process=image/watermark,image_L21pY3JvL0xldmlhdGhhbi9iYWNrZW5kL3dhdGVybWFya2VyLnBuZw==,g_center,t_50/resize,w_700'
+      url = this.data.lessonImgs[index].imgObj.url + '?x-image-process=image/resize,p_50/watermark,image_L21pY3JvL0xldmlhdGhhbi9iYWNrZW5kL3dhdGVybWFya2VyLnBuZw==,g_center,t_50/resize,w_700'
     wx.previewImage({
       urls: [url]
     })
